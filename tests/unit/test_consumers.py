d4024e60a491983cb10d8390a550495f1d44b304
Victor Lin <bornstub@gmail.com>
2014-09-25 11:26:46 -0700

    More tests for consumer hub

--- tests/unit/test_consumers.py
+++ tests/unit/test_consumers.py
@@ -12,8 +12,19 @@ class TestConsumer(unittest.TestCase):
         hub.scan(my_consumers)
 
         self.assertEqual(len(hub.consumers), 2)
-        funcs = set(consumer.func for consumer in hub.consumers)
+        func_to_consumers = dict(
+            (consumer.func, consumer)
+            for consumer in hub.consumers
+        )
         self.assertEqual(
-            funcs,
+            set(func_to_consumers.keys()),
             set([my_consumers.consumer_a, my_consumers.consumer_b]),
         )
+
+        consumer_a = func_to_consumers[my_consumers.consumer_a]
+        self.assertEqual(consumer_a.topic, 'foo.bar')
+        self.assertEqual(consumer_a.cls_types, ('eggs', ))
+
+        consumer_b = func_to_consumers[my_consumers.consumer_b]
+        self.assertEqual(consumer_b.topic, 'spam.eggs')
+        self.assertEqual(consumer_b.cls_types, None)
